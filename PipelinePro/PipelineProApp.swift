ea0acfca139c13393ed88fa4ac61a52335bdfca2
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-07-08 13:14:54 +0530

    Initial Commit

---
+++ PipelinePro/PipelineProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineProApp.swift
+//  PipelinePro
+//
+//  Created by Swarajmeet Singh on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PipelineProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PipelineProTests/PipelineProTests.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineProTests.swift
+//  PipelineProTests
+//
+//  Created by Swarajmeet Singh on 08/07/25.
+//
+
+import Testing
+@testable import PipelinePro
+
+struct PipelineProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
